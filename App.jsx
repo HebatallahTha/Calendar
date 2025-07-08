1af0d35be51971f84abf10106257212507546b8e
HebatallahTha <168129594+HebatallahTha@users.noreply.github.com>
2025-07-08 18:42:19 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Calendar from './components/Calendar'
+
+export function App(props) {
+  return (
+    <div className='App'>
+      <h1>🎀 A Week in the Life of Heba!
+</h1>
+      <h2>Join me on my journey as a CodePath student, Software Engineer Intern, and caring daughter and sister — balancing tech, family, and fun every day! ✨</h2>
+      <Calendar />
+
+    </div>
+  );
+}
+
+// Log to console
+console.log('Hello console')
